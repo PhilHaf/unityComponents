9f9edba125c90861cc144d93e591cc60ee6b1173
Philipp H <philipp.h@i.graphics>
2019-04-14 00:28:31 +0200

    init

---
+++ BezierCurvePointRenderer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BezierCurvePointRenderer : MonoBehaviour {
+
+    // Use this for initialization
+    [Tooltip("constant update of given Points")]
+    public bool updatePoints;
+    [Tooltip("Line Renderer required")]
+    public LineRenderer lineRenderer;
+    public int vertexCount;
+    [Tooltip("shown just for debugging, min 2 Points are required")]
+    public Transform[] points;
+
+    private Vector3 lastPoint;
+
+	void Start () {
+        
+        points = gameObject.GetComponentsInChildren<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (updatePoints) {
+
+            points = gameObject.GetComponentsInChildren<Transform>();
+        }
+
+        var pointList = new List<Vector3>();
+
+        if (points.Length > 2) {
+
+            for (int i = 0; i <= points.Length-1; i++) {
+
+                if (i == 0 || i == points.Length-1) {
+                    
+                    pointList.Add(points[i].position);
+
+                    lastPoint = points[i].position;
+
+                }else {
+                   
+                    for (float ratio = 0.0f / vertexCount; ratio <= 1; ratio += 1.0f / vertexCount)
+                    {
+
+                        var tangentLineVertex1 = Vector3.Lerp(lastPoint, points[i].position, ratio);
+                       
+                        var tangentLineVertex2 = Vector3.Lerp(points[i].position, points[i+1].position, ratio);
+
+                        var bezierPoint = Vector3.Lerp(tangentLineVertex1, tangentLineVertex2, ratio);
+
+                        pointList.Add(bezierPoint);
+
+                        lastPoint = bezierPoint;
+                    }
+                }
+            }
+
+        } else if (points.Length == 2){ 
+
+            pointList.Add(points[0].position);
+            pointList.Add(points[1].position);
+        }
+
+        lineRenderer.positionCount = pointList.Count;
+        lineRenderer.SetPositions(pointList.ToArray());
+	}
+}
---
+++ UpdateServerAssets.cs
@@ -0,0 +1,129 @@
+﻿/* 
+ * define model sprite tex 
+ * _schleichmodel
+ * _schleichtex
+ * _schleichsprite
+ * 
+ * _mod
+ * _mat
+ * _tex
+ * _spr
+ * 
+ * create materials
+ * hard replace
+ * else update if file is older
+ * 
+ */
+
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+
+public class UpdateServerAssets : ScriptableWizard
+{
+    static string extPath = null;
+
+    public static bool hardReplace = true;
+
+    public static bool updMod = true;
+    public static bool updTex = true;
+    public static bool updSpr = true;
+
+    public static string extMod;
+    public static string extTex;
+    public static string extSpr;
+
+    public static string intMod;
+    public static string intTex;
+    public static string intSpr;
+
+    public string setextMod = "_mod";
+    public string setextTex = "_tex";
+    public string setextSpr = "_spr";
+				  
+    public string setintMod = "_mod";
+    public string setintTex = "_tex";
+    public string setintSpr = "_spr";
+
+    public bool setupdMod = true;
+    public bool setupdTex = true;
+    public bool setupdSpr = true;
+    public bool setHardReplace = true;
+    public string setPath = "";
+
+
+    [MenuItem("My Tools/Update With Server Assets...")]
+    static void CreateWizard()
+    {
+        // Creates the wizard for display
+        ScriptableWizard.DisplayWizard("Title", typeof(UpdateServerAssets), "Execute", "Folder");
+    }
+
+    void OnWizardUpdate()
+    {
+        extPath = setPath;
+        hardReplace = setHardReplace;
+
+        updMod = setupdMod;
+        extMod = setextMod;
+        intMod = setintMod;
+
+        updTex = setupdTex;
+        extTex = setextTex;
+        intTex = setintTex;
+
+        updSpr = setupdSpr;
+        extSpr = setextSpr;
+        intSpr = setintSpr;
+    }
+
+    void OnWizardCreate()
+    {
+
+        if (hardReplace) {
+
+            if (updMod) {
+
+                replaceElements(extMod, intMod);
+            }
+            if (updTex)
+            {
+
+                replaceElements(extTex, intTex);
+            }
+            if (updSpr)
+            {
+
+                replaceElements(extSpr, intSpr);
+            }
+        }
+    }
+
+    void replaceElements(string exFolder, string inFolder) {
+     
+        string[] files = Directory.GetFiles(extPath +"/"+ exFolder);
+
+        foreach (string file in files)
+        {
+          
+            if (hardReplace) {
+
+                File.Copy(file, Application.dataPath + "/" + inFolder + "/" + Path.GetFileName(file), true);
+            } else {
+
+                if (File.GetLastWriteTime(file)> File.GetLastWriteTime(Application.dataPath + "/" + inFolder + "/" + Path.GetFileName(file))) {
+
+                    File.Copy(file, Application.dataPath + "/" + inFolder + "/" + Path.GetFileName(file), true);
+                }
+            }
+        }
+    }
+
+
+    void OnWizardOtherButton()
+    {
+        setPath = EditorUtility.OpenFolderPanel("Select Project Folder on Server", "", "");
+    }
+
+}
